6cb45cc2746f512fb4003596034ef67c02e596e5
BenChan <chenjiabin@hotmail.com>
2018-07-25 22:55:22 +0800

    提交代码

--- app/src/main/java/team/benchem/layoutdemo/FragmentDemoActivity.java
+++ app/src/main/java/team/benchem/layoutdemo/FragmentDemoActivity.java
@@ -1,8 +1,14 @@
 package team.benchem.layoutdemo;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+
+import team.benchem.layoutdemo.databinding.ModuleActivityFragmentdemoBinding;
 
 public class FragmentDemoActivity extends Activity {
 
@@ -10,5 +16,22 @@ public class FragmentDemoActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.module_activity_fragmentdemo);
+
+        ModuleActivityFragmentdemoBinding binding = DataBindingUtil.setContentView(this, R.layout.module_activity_fragmentdemo);
+        binding.setEventHandler(this);
+    }
+
+    public void onSend(View view){
+
+        //方法1
+        TextView textView = (TextView) findViewById(R.id.module_fragment_left_tvMemo);
+        textView.setText("AAAAA1");
+
+        //方法2
+        RightFragment fragment = RightFragment.newInstance("AAAAA2");
+        FragmentTransaction trans =  getFragmentManager().beginTransaction();
+        trans.replace(R.id.module_activity_fragmentdemo_rightFragment, fragment);
+        trans.commit();
+
     }
 }
--- app/src/main/java/team/benchem/layoutdemo/RightFragment.java
+++ app/src/main/java/team/benchem/layoutdemo/RightFragment.java
@@ -1,18 +1,41 @@
 package team.benchem.layoutdemo;
 
 import android.app.Fragment;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RightFragment extends Fragment {
+
+    static RightFragment newInstance(String text){
+
+        RightFragment fragment = new RightFragment();
+
+        Bundle args = new Bundle();
+        args.putString("text", text);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    String initText;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initText = getArguments() != null ? getArguments().getString("text") : "Rec....";
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.module_fragment_right, null);
+        TextView textView =  view.findViewById(R.id.module_fragment_right_tvMemo);
+        textView.setText(initText);
         return view;
     }
 }
