01d57967327f3fa0fb1da8ce8bc7106acd362d59
BenChan <chenjiabin@hotmail.com>
2018-07-26 07:50:50 +0800

    调整代码

--- app/src/main/java/team/benchem/layoutdemo/FragmentDemoActivity.java
+++ app/src/main/java/team/benchem/layoutdemo/FragmentDemoActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 import team.benchem.layoutdemo.databinding.ModuleActivityFragmentdemoBinding;
 
+/**
+ * https://developer.android.com/reference/android/app/Fragment
+ */
 public class FragmentDemoActivity extends Activity {
 
     @Override
@@ -31,7 +34,8 @@ public class FragmentDemoActivity extends Activity {
         RightFragment fragment = RightFragment.newInstance("AAAAA2");
         FragmentTransaction trans =  getFragmentManager().beginTransaction();
         trans.replace(R.id.module_activity_fragmentdemo_rightFragment, fragment);
+        trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        trans.addToBackStack(null);
         trans.commit();
-
     }
 }
