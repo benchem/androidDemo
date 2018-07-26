1cf491c1b78e801ce812384b60f23ce0cc5e792d
BenChan@OfficePC <chenjiabin@lonntec.com>
2018-07-26 11:13:59 +0800

    调整代码

--- app/src/main/java/team/benchem/layoutdemo/FragmentDemoActivity.java
+++ app/src/main/java/team/benchem/layoutdemo/FragmentDemoActivity.java
@@ -1,10 +1,10 @@
 package team.benchem.layoutdemo;
 
-import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,7 +13,7 @@ import team.benchem.layoutdemo.databinding.ModuleActivityFragmentdemoBinding;
 /**
  * https://developer.android.com/reference/android/app/Fragment
  */
-public class FragmentDemoActivity extends Activity {
+public class FragmentDemoActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,11 +31,11 @@ public class FragmentDemoActivity extends Activity {
         textView.setText("AAAAA1");
 
         //方法2
-        RightFragment fragment = RightFragment.newInstance("AAAAA2");
-        FragmentTransaction trans =  getFragmentManager().beginTransaction();
-        trans.replace(R.id.module_activity_fragmentdemo_rightFragment, fragment);
-        trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        trans.addToBackStack(null);
-        trans.commit();
+//        RightFragment fragment = RightFragment.newInstance("AAAAA2");
+//        FragmentTransaction trans =  getFragmentManager().beginTransaction();
+//        trans.replace(R.id.module_activity_fragmentdemo_rightFragment, fragment);
+//        trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//        trans.addToBackStack(null);
+//        trans.commit();
     }
 }
